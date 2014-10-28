dda633ad4debacf73f8f33b75a6221d7d0433d26
Ryan Santos <rsantos.sjsu@gmail.com>
2014-10-27 20:59:40 -0700

    Update index with input fields / update JS
    Input, header, answer block, button, and button click recognition.

---
+++ js/random.js
@@ -0,0 +1,24 @@
+$(function(){
+    var number1 = $("first_value").val();
+    var number2 = $("second_value").val();
+    var button = $("button#solutionbutton");
+    
+    button.on("click",function(){
+        
+        console.log("Solution Clicked!"); 
+    
+    
+    
+    
+    })
+    
+    
+    
+
+
+
+
+
+
+
+});
