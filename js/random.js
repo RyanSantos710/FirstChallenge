ea454592e96ddeb9851e324927b5d2215ec08f99
Ryan Santos <rsantos.sjsu@gmail.com>
2014-10-27 22:59:40 -0700

    Add JS code / Functioning Addition Code
    Non-CSS functioning addition code. Stable

--- js/random.js
+++ js/random.js
@@ -1,24 +1,16 @@
-$(function(){
-    var number1 = $("first_value").val();
-    var number2 = $("second_value").val();
+$(function(){    
     var button = $("button#solutionbutton");
-    
     button.on("click",function(){
-        
-        console.log("Solution Clicked!"); 
-    
-    
-    
-    
+        var number1 = ($("#first_value").val())|| "0";
+        var number2 = ($("#second_value").val()) || "0";
+        var answer = parseInt(number1) + parseInt(number2);
+        console.log("Solution Clicked!");
+        console.log(number1);
+        console.log(number2);
+        console.log(answer);
+        $("div#answer").text(answer);
+        $("input").val(''); //clears input field after button is clicked
     })
     
     
-    
-
-
-
-
-
-
-
 });
