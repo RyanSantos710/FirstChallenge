3f44267002098aaa6c0f3e8dd46643c3e9d9d74f
Ryan Santos <rsantos.sjsu@gmail.com>
2014-10-27 23:50:09 -0700

    Add Padding / Add Hiding Answers
    Add the spacing so that the text isn’t pushed against the window. Add
    hiding the answer and the text before the solution is entered.

--- js/random.js
+++ js/random.js
@@ -1,7 +1,8 @@
-$(function(){    
+$(function(){
+    
     var button = $("button#solutionbutton");
     button.on("click",function(){
-        var number1 = ($("#first_value").val())|| "0";
+        var number1 = ($("#first_value").val()) || "0";
         var number2 = ($("#second_value").val()) || "0";
         var answer = parseInt(number1) + parseInt(number2);
         console.log("Solution Clicked!");
@@ -9,7 +10,9 @@ $(function(){
         console.log(number2);
         console.log(answer);
         $("div#answer").text(answer);
+        $(".solution").show();
         $("input").val(''); //clears input field after button is clicked
+        
     })
     
     
