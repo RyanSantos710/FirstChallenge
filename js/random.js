6ed2b71f77b10d85f14bfcf3836df38549db5aab
Ryan Santos <rsantos.sjsu@gmail.com>
2014-10-28 10:30:02 -0700

    Add Enter Input Feature / CSS Center, Color Change
    Remove the button and now triggers on enter input (works on both
    fields). Centered text to get away from the border edges. Change colors
    to show text color isolation. Changed padding/margins to press words
    together.

--- js/random.js
+++ js/random.js
@@ -1,19 +1,16 @@
 $(function(){
-    
-    var button = $("button#solutionbutton");
-    button.on("click",function(){
-        var number1 = ($("#first_value").val()) || "0";
-        var number2 = ($("#second_value").val()) || "0";
-        var answer = parseInt(number1) + parseInt(number2);
-        console.log("Solution Clicked!");
-        console.log(number1);
-        console.log(number2);
-        console.log(answer);
-        $("div#answer").text(answer);
-        $(".solution").show();
-        $("input").val(''); //clears input field after button is clicked
-        
-    })
-    
-    
-});
+    $("input").on("keyup", function(e){                         //when enter is pressed, execute
+        if(e.keyCode === 13){                                   //keyCode for "enter" is keyCode === 13
+            var $this = $(this);                                //stores the specific event to this
+            var number1 = ($("#first_value").val()) || "0";     //assign variable OR assign it to 0
+            var number2 = $this.val() || "0";                   //assign variable OR assign it to 0
+            var answer = parseInt(number1) + parseInt(number2);
+            console.log(number1);                               //check number1
+            console.log(number2);                               //check number2
+            console.log(answer);                                //check if answer is correct
+            $("div#answer").text(answer);                       //display answer
+            $(".solution").show();                              //shows text "answer" when an answer is present
+            $("input").val('');                                 //clears input field after button is clicked
+        }                                                       //end of if statement
+    });                                                         // end of input and function
+});                                                             //end of function
